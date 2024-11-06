a89c36bb7f11c68a81acfe7580d3f78267e679c1
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2024-11-06 12:43:00 +0300

    First Section of Skinet-Ecommerce Done

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+using System.Reflection.Metadata.Ecma335;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+   
+    public class ProductsController(StoreContext context) : BaseApiController
+    {
+         
+         [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+           return await context.Products.ToListAsync();
+        }
+
+         [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+          var product = await context.Products.FindAsync(id);
+
+          if(product is null) return NotFound();
+          return product;
+        }
+
+
+          [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+
+               context.Products.Add(product);
+               await context.SaveChangesAsync();
+           
+               return product;
+        }
+
+         [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(int id, Product product)
+        {
+            if(product.Id != id || !ProductExist(id))
+             return BadRequest("Cannot update the products");
+             
+          
+               context.Entry(product).State = EntityState.Modified;
+               await context.SaveChangesAsync();
+           return NoContent();
+        }
+
+          [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+          var product = await context.Products.FindAsync(id);
+
+          if(product is null) return NotFound();
+
+           context.Products.Remove(product);
+            await context.SaveChangesAsync();
+             return NoContent();
+        }
+
+        private bool ProductExist(int id)
+        {
+           return context.Products.Any(x => x.Id == id);
+        }
+    }
+}
---
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Config
+{
+    /// <summary>
+    /// this class handle the classes property configuration instead of
+    /// override Onmodelcreated method in db
+    /// </summary>
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+            builder.Property(x => x.Name).IsRequired();
+        }
+    }
+}
