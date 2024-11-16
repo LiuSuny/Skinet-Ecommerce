260481c5652517a057e54296116120ec7875f09f
Sunday A <119320935+LiuSuny@users.noreply.github.com>
2024-11-16 11:20:08 +0300

    Customize error handling all done on client side

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 import { SnarkbarService } from '../services/snarkbar.service';
 
@@ -11,9 +11,24 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(req).pipe(
     catchError((err: HttpErrorResponse) =>{
+
       if(err.status === 400){
+        if(err.error.errors)
+        {
+          const modelStateError =[];
+          for(const key in err.error.errors){
+             if(err.error.errors[key]){
+               modelStateError.push(err.error.errors[key]) //the idea is to flatten the--- 
+               //error response we get back from our validation response and push then into array modelStateError =[];
+             }
+        }
+        throw modelStateError.flat();
+      } 
+      else {
         snackbarService.error(err.error.title || err.error);
+       }
       }
+
       if(err.status === 401){
         snackbarService.error(err.error.title || err.error);
       }
@@ -21,7 +36,8 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
         router.navigateByUrl('/not-found');
       }
       if(err.status === 500){
-        router.navigateByUrl('/server-error');
+        const navigationExtra: NavigationExtras = {state:{error: err.error}}
+        router.navigateByUrl('/server-error', navigationExtra);
       }
       return throwError(() => err);
     })
