d27327f1d58b3f77c0b3a4b3a47378222c341d1f
Sunday A <119320935+LiuSuny@users.noreply.github.com>
2024-11-17 15:30:42 +0300

    Client Shopping Cart all done using updated angular features

---
+++ client/src/app/core/services/cart.service.ts
@@ -0,0 +1,140 @@
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Cart, CartItem } from '../../shared/models/cart';
+import { Product } from '../../shared/models/product';
+import { map } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  baseUrl = environment.apiUrl;
+  
+  private http = inject(HttpClient);
+
+  cart = signal<Cart | null>(null); //using signal
+  //getting the total count of item add to cart with signal computed
+  itemCount = computed(() => {
+    return this.cart()?.items.reduce((sum, item) => sum + item.quantity, 0);
+  })
+ 
+  //total order summery of item using reduce fron js
+  totals = computed(() => {
+       const cart = this.cart();
+       if(!cart)return null;
+       const subtotal = cart.items.reduce((sum, item) =>  sum + item.price * item.quantity, 0);
+       const shipping = 0;
+       const discount = 0;
+      
+       return {
+        subtotal,
+        shipping,
+        discount,
+        total: subtotal + shipping - discount
+       }
+  })
+
+
+
+  getCart(id: string){
+    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(
+      map(cart => {
+        this.cart.set(cart)
+        return cart;
+      })
+    )
+  }
+
+//this method update the cart on the redis server
+  setCart(cart: Cart){
+    return this.http.post<Cart>(this.baseUrl + 'cart', cart).subscribe({
+      next: cart => this.cart.set(cart)
+    })
+  }
+
+  //adding product to cart
+  addItemToCart(item: CartItem | Product, quantity = 1) {
+    //getting of the product items and if not create a cart
+    const cart = this.cart() ?? this.createCart();
+    if(this.isProduct(item)){
+      item = this.mapProductToCartItem(item)
+    }
+    
+    cart.items = this.addOrUpdateItem(cart.items, item, quantity);
+     return this.setCart(cart); //returning the signal
+  }
+
+  //empty the cart
+  removeItemFromCart(productId: number, quantity = 1){
+    const cart = this.cart();
+    if(!cart)return;
+
+    const index = cart.items.findIndex(x => x.productId === productId);
+
+    if(index !== -1){
+
+      if(cart.items[index].quantity > quantity){
+        cart.items[index].quantity -= quantity; //reducing the quantity if it greater than the default one 
+      }else{
+        cart.items.splice(index, 1) //removing 
+      }
+
+      if(cart.items.length === 0){
+        this.deleteCart();
+      }else {
+        this.setCart(cart)
+      }
+    }
+   
+  }
+ //removing from redis serve side
+  deleteCart() {
+    return this.http.delete(this.baseUrl + 'cart?id=' + this.cart()?.id).subscribe({
+      next: () => {
+        localStorage.removeItem('cart_id');
+        this.cart.set(null);
+      }
+    })
+  }
+  
+  private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
+     const index = items.findIndex(x => x.productId === item.productId);
+     if(index === -1){
+      item.quantity = quantity;
+      items.push(item); //sending to cart
+     } else{
+      items[index].quantity +=quantity; //to increase the amount in the basket
+     }
+
+     return items;
+  }
+
+  //mapping product to cartitem just dto sort of
+  private mapProductToCartItem(item: Product): CartItem {
+     return {
+        productId: item.id,
+        productName: item.name,
+        price: item.price,
+        quantity : 0,
+        pictureUrl: item.pictureUrl,
+        brand: item.brand,
+        type: item.type
+     }
+  }
+
+  private isProduct(item: CartItem | Product): item is Product {
+    //the idea of this check if the item has id property b/c product has id property and the cartitem is not 
+    //then that is not equal to undefined and item is a product and product is going to return true 
+      return (item as Product).id !==undefined; 
+  }
+
+  //create a cart 
+  private createCart(): Cart {
+    const cart = new Cart();
+    localStorage.setItem('cart_id', cart.id);
+    return cart;
+  }
+}
--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -8,6 +8,8 @@ import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatDivider } from '@angular/material/divider';
+import { CartService } from '../../../core/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -19,7 +21,8 @@ import { MatDivider } from '@angular/material/divider';
     MatFormField,
     MatInput,
     MatLabel,
-    MatDivider
+    MatDivider,
+    FormsModule
   ],
   templateUrl: './product-details.component.html', 
   styleUrl: './product-details.component.scss'
@@ -27,9 +30,12 @@ import { MatDivider } from '@angular/material/divider';
 export class ProductDetailsComponent implements OnInit {
 
   private shopService = inject(ShopService);
+  private cartService = inject(CartService);
   private activeRoute = inject(ActivatedRoute);
 
   product?: Product;
+  quantityInCart = 0;
+  quantity = 1;
 
   ngOnInit(): void {
     this.loadProduct();
@@ -40,9 +46,35 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activeRoute.snapshot.paramMap.get('id'); //note: this match id which was initialize in app.route 'shop/:id',
      if(!id) return;
      this.shopService.getProductById(+id).subscribe({
-      next: response => this.product = response,
+      next: response => {this.product = response,
+                          this.updateQuantityInCart();
+                        },
       error: error=> console.log(error)
      })
+  }
+ 
+  updateCart(){
+    if(!this.product)return;
+    if(this.quantity > this.quantityInCart){
+      const itemToAdd  = this.quantity - this.quantityInCart; //this check if the quantity is greater than what is in cart
+      this.quantityInCart += itemToAdd;
+      this.cartService.addItemToCart(this.product, itemToAdd);
+    } else {
+      const removeItem  = this.quantityInCart - this.quantity;
+      this.quantityInCart -= removeItem;
+      this.cartService.removeItemFromCart(this.product.id, removeItem);
+    }
+  }
+  //updating the quantity of products in cart 
+  updateQuantityInCart(){
+    this.quantityInCart = this.cartService.cart()?.items
+    .find(x=> x.productId ===this.product?.id)?.quantity || 0;
+    this.quantity = this.quantityInCart || 1;
+
+  }
+
+  getButtonText(){
+    return this.quantityInCart > 0 ? 'Update cart ' : 'Add to cart';
   }
 
 }
