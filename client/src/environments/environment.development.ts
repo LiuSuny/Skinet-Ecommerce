df1835c1e2f1625f8c3822754262507fc5f1916f
Sunday A <119320935+LiuSuny@users.noreply.github.com>
2026-01-13 21:33:54 +0100

    pre continous integration

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
     hubUrl: 'https://localhost:5001/hub/notifications',
-    stripePublicKey: 'pk_test_51QAAtSGLBwywGm5MzA755RvPQ3C16OYwIDh5hgpvO2fjdze1AeOdHhDL9QFpzWvksAeCSJtstZREVh50eVKRG3Zc00UOEfu43i'
+    stripePublicKey: 'pk_test_51Sp7RiGxlOW9VGXKIRXY4AnCcelrVSDwgmHVJj1UvSyop1GhUGf7HRa1Me6XygzpyYlY14DAhzUoOGKPZr17Bmi500ajDoiU52'
 };
